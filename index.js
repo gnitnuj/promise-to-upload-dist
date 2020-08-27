e67f75b7f10c6cd98162697adc0196cd70d8527a
Junting Lu <junting.lu@gmail.com>
2020-08-27 05:40:59 -0700

    index.js - trivial formatting

--- index.js
+++ index.js
@@ -11,80 +11,80 @@ const getContentType = require("content-type");
 // winston/logging configuration
 const tsFormat = () => new Date().toLocaleTimeString();
 const logger = winston.createLogger({
-    transports: [
-        // colorize the output to the console
-        new winston.transports.Console({
-            timestamp: tsFormat,
-            colorize: true,
-        }),
-    ],
+  transports: [
+    // colorize the output to the console
+    new winston.transports.Console({
+      timestamp: tsFormat,
+      colorize: true,
+    }),
+  ],
 });
 
 // helper function to flatten array
 const flattenDeep = (arr) => {
-    return arr.reduce(
-        (acc, val) =>
-            Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val),
-        []
-    );
+  return arr.reduce(
+    (acc, val) =>
+      Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val),
+    []
+  );
 };
 
 module.exports = (bucket, hash) => {
-    const distDir = path.resolve("./dist");
+  const distDir = path.resolve("./dist");
 
-    // If hash is not provided (local env deploy), assume travis
-    hash = hash ? hash : process.env.TRAVIS_COMMIT;
+  // If hash is not provided (local env deploy), assume travis
+  hash = hash ? hash : process.env.TRAVIS_COMMIT;
 
-    logger.info("uploading dist/");
+  logger.info("uploading dist/");
 
-    return accumulateFiles(distDir)
-        .then((results) => {
-            return Promise.all(
-                flattenDeep(results).map((filePath) => {
-                    return util
-                        .promisify(fs.readFile)(filePath)
-                        .then((fileData) => {
-                            return { filePath, fileData };
-                        });
-                })
-            );
+  return accumulateFiles(distDir)
+    .then((results) => {
+      return Promise.all(
+        flattenDeep(results).map((filePath) => {
+          return util
+            .promisify(fs.readFile)(filePath)
+            .then((fileData) => {
+              return { filePath, fileData };
+            });
         })
-        .then((data) => {
-            // Current & Build promises seperated for clarity
-            const currentPromises = data.map((d) => {
-                const key = path.join(
-                    "container/3.x/current/",
-                    path.relative(path.resolve(distDir), d.filePath)
-                );
-                const contentType = getContentType(d.filePath);
-                logger.info(`uploaded file: ${bucket}/${key}`);
-                return s3.uploadFile(
-                    bucket,
-                    key,
-                    d.fileData,
-                    contentType,
-                    "public-read"
-                );
-            });
+      );
+    })
+    .then((data) => {
+      // Current & Build promises seperated for clarity
+      const currentPromises = data.map((d) => {
+        const key = path.join(
+          "container/3.x/current/",
+          path.relative(path.resolve(distDir), d.filePath)
+        );
+        const contentType = getContentType(d.filePath);
+        logger.info(`uploaded file: ${bucket}/${key}`);
+        return s3.uploadFile(
+          bucket,
+          key,
+          d.fileData,
+          contentType,
+          "public-read"
+        );
+      });
 
-            const buildPromises = data.map((d) => {
-                const key = path.join(
-                    `container/3.x/builds/${hash}/`,
-                    path.relative(path.resolve(distDir), d.filePath)
-                );
-                const contentType = getContentType(d.filePath);
-                logger.info(`uploaded file: ${bucket}/${key}`);
-                return s3.uploadFile(
-                    bucket,
-                    key,
-                    d.fileData,
-                    contentType,
-                    "public-read"
-                );
-            });
+      const buildPromises = data.map((d) => {
+        const key = path.join(
+          `container/3.x/builds/${hash}/`,
+          path.relative(path.resolve(distDir), d.filePath)
+        );
+        const contentType = getContentType(d.filePath);
+        logger.info(`uploaded file: ${bucket}/${key}`);
+        return s3.uploadFile(
+          bucket,
+          key,
+          d.fileData,
+          contentType,
+          "public-read"
+        );
+      });
 
-            const promises = [...buildPromises, ...currentPromises];
+      const promises = [...buildPromises, ...currentPromises];
 
-            return Promise.all(promises);
-        });
+      return Promise.all(promises);
+    });
 };
